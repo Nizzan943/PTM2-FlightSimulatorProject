9d6d9e0b8d316a50125e77b2f79c75db4ac59b78
oriba <oribachar98@gmail.com>
2021-04-17 22:15:51 +0300

    GUI

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -1,4 +1,20 @@
 package View;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javax.swing.text.html.ListView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable
+{
+    @FXML private ListView listView;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+
+    }
 }
