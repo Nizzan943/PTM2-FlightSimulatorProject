4ac3332a332e851bce7214141a6350cddaf88a26
yuvalamir123 <yuval.amir998@gmail.com>
2021-04-18 00:51:42 +0300

    open file function (not connected to the button)

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -2,12 +2,15 @@ package View;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Controller {
@@ -29,5 +32,18 @@ public class Controller {
         listView.getItems().add("niz");
         listView.getItems().add("ori");
         listView.refresh();
+    }
+
+    public void openCSV()
+    {
+        FileChooser fc = new FileChooser();
+        fc.setTitle("open csv file"); //headline
+        fc.setInitialDirectory(new File("./resources")); //what happens when we click
+        File chozen = fc.showOpenDialog(null);
+        if (chozen != null)
+        {
+            System.out.println(chozen.getName());
+        }
+
     }
 }
