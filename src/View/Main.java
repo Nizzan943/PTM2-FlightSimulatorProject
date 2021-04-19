3f199f9e395277b66fd83fd864750d724befd29f
oriba <oribachar98@gmail.com>
2021-04-19 10:48:42 +0300

    Change app size

--- src/View/Main.java
+++ src/View/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
         primaryStage.setTitle("Flight Simulator");
-        primaryStage.setScene(new Scene(root, 600  , 400));
+        primaryStage.setScene(new Scene(root, 797  , 571));
         primaryStage.show();
     }
 
