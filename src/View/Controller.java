02d60e2672c5c1f98c13c1dac3a13515e27be16f
yuvalamir123 <yuval.amir998@gmail.com>
2021-04-19 10:37:58 +0300

    change Alert messages

--- src/View/Controller.java
+++ src/View/Controller.java
@@ -33,6 +33,7 @@ public class Controller {
     public void openCSV() {
         int flag = 0;
         FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
         fc.setTitle("Open CSV file"); //headline
         fc.setInitialDirectory(new File("/")); //what happens when we click
         File chozen = fc.showOpenDialog(null);
@@ -40,8 +41,9 @@ public class Controller {
             TimeSeries timeSeries = new TimeSeries(chozen.getAbsolutePath());
             if (timeSeries.getCols().length != 2) {
                 JOptionPane.showMessageDialog(null,
-                        "Missing Argument, please check your file and try again",
-                        "ERROR",
+                        "Missing Arguments\n" +
+                                "Please check your file and try again",
+                        "Error",
                         JOptionPane.ERROR_MESSAGE);
             }
             for (int i = 0; i < timeSeries.getCols().length; i++)
@@ -50,10 +52,12 @@ public class Controller {
                 if (timeSeries.getCols()[i].getName().intern() != XML_settings.PropertyList.get(i).getRealName().intern())
                 {
                     JOptionPane.showMessageDialog(null,
-                            "Incompatibility with XML file, please check your file and try again",
-                            "ERROR",
+                            "Incompatibility with XML file\n" +
+                                    "Please check your file and try again",
+                            "Error",
                             JOptionPane.ERROR_MESSAGE);
                     flag = 1;
+                    break;
                 }
             }
             if (timeSeries.getCols().length == 2 && flag == 0) {
@@ -67,8 +71,9 @@ public class Controller {
     public void WrongFormatAlert()
     {
         JOptionPane.showMessageDialog(null,
-                "Wrong format of file, please check your format and try again",
-                "ERROR",
+                "Wrong format of XML\n" +
+                        "Please check your format and try again",
+                "Error",
                 JOptionPane.ERROR_MESSAGE);
 
     }
@@ -76,8 +81,9 @@ public class Controller {
     public void MissingArgumentAlert()
     {
         JOptionPane.showMessageDialog(null,
-                "Missing Argument, please check your settings and try again",
-                "ERROR",
+                "Missing ArgumentS\n" +
+                        "Please check your settings and try again",
+                "Error",
                 JOptionPane.ERROR_MESSAGE);
     }
 
@@ -91,6 +97,7 @@ public class Controller {
 
     public void LoadXML() throws Exception {
         FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("XML Files", "*.xml"));
         fc.setTitle("Load XML file"); //headline
         fc.setInitialDirectory(new File("/")); //what happens when we click
         File chozen = fc.showOpenDialog(null);
