e31582fa0c5a94db094ceb12fef5f3798f97923d
oriba <oribachar98@gmail.com>
2021-04-19 14:04:07 +0300

    Main that flight the plane

--- src/Model/MainTrain.java
+++ src/Model/MainTrain.java
@@ -1,9 +1,24 @@
 package Model;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class MainTrain
 {
     public static void main(String[] args) throws Exception {
-        HandleXML handleXML = new HandleXML();
-        handleXML.deserializeFromXML("C:\\Users\\yuval\\Desktop\\yuval2.xml");
+        Socket fg=new Socket("localhost", 5400);
+        BufferedReader in=new BufferedReader(new FileReader("D:\\Repos\\PTM2-FlightSimulatorProject\\Files\\reg_flight.csv"));
+        PrintWriter out=new PrintWriter(fg.getOutputStream());
+        String line;
+        while((line=in.readLine())!=null) {
+            out.println(line);
+            out.flush();
+            Thread.sleep(20);
+        }
+        out.close();
+        in.close();
+        fg.close();
     }
 }
