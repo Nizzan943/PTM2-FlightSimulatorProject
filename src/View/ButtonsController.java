d7b2ec2d6e4c24a0cf0bb9bf3482e13cf0c348aa
Nitzan Miranda <Nizzan943@gmail.com>
2021-04-19 21:15:17 +0300

    GUI and Buttons fix

--- src/View/ButtonsController.java
+++ src/View/ButtonsController.java
@@ -2,17 +2,60 @@ package View;
 
 import Model.TimeSeries;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 
 import javax.swing.*;
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ButtonsController
-{
-    public void Add()
-    {
-        System.out.println("Yuv");
+public class ButtonsController implements Initializable {
+    @FXML
+    private ChoiceBox playSpeedDropDown;
+    @FXML
+    static Button play;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        playSpeedDropDown.getItems().addAll("x0.5", "x1", "x1.5", "x2.0");
+        //String[] buttonLabels = {"\u25b6 Play", "\u23f8 Pause", "\u23F9 Stop", "\u23EA Fast Backward", "\u23E9  Fast Forward"};
+        //play.setBackground(new Background(new BackgroundImage(new Image(getClass().getResource("Images/Play.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT)));
+        //String absolutePathToIcon =
+        //        getClass().getResource("Play.png").toExternalForm();
+        //play.setGraphic(new ImageView(new Image("file:View\\Play.png")));
     }
+
+    public void Play(ActionEvent event) {
+        System.out.println("Play");
+    }
+
+    public void Pause(ActionEvent event) {
+        System.out.println("Pause");
+    }
+
+    public void FastForward(ActionEvent event) {
+        System.out.println("FastForward");
+    }
+
+    public void FastBackward(ActionEvent event) {
+        System.out.println("FastBackward");
+    }
+
+    public void Plus15(ActionEvent event) {
+        System.out.println("+15");
+    }
+
+    public void Minus15(ActionEvent event) {
+        System.out.println("-15");
+    }
+
 }
