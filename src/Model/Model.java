4e17cb9e2be4f833fd78ead4bc8af2bccadab236
oriba <oribachar98@gmail.com>
2021-04-19 14:04:41 +0300

    Model class

---
+++ src/Model/Model.java
@@ -0,0 +1,37 @@
+package Model;
+
+import java.util.HashMap;
+import java.util.Observable;
+import java.util.Observer;
+
+public class Model extends Observable implements Observer
+{
+    private SimulatorClient simulatorClient;
+
+
+    public void setAileron(float aileron)
+    {
+
+    }
+
+    public void setElevators(float elevators)
+    {
+
+    }
+
+    public void setRudder(float rudder)
+    {
+
+    }
+
+    public void setThrottle(float throttle)
+    {
+
+    }
+
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
